8e2a55aeeae496ae9f6d07c94e590eb930cf1ef4
Kanzaki-Morrison <celina.kanzakimorris@uleth.ca>
2019-03-10 18:21:18 -0600

    first commit

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -9,3 +9,31 @@ var app = new Framework7({
 })
 
 var mainView = app.views.create('.view-main');
+
+document.addEventListener("deviceready", init, false);
+
+function init() {
+        //camera code goes here
+
+    $("#takePic").on("click", function () {
+        console.log("user tapped")
+        navigator.camera.getPicture(cameraSuccess, cameraFail, cameraOptions);
+    })
+
+    var cameraOptions = {
+        //number range 0-100, default is 50
+        quality: 80,
+        //default is back
+        saveToPhotoAlbum: true
+    }
+   
+    function cameraSuccess(imageURI) {
+        console.log("great pic!");
+        $("#takePic").after("<img src='" + imageURI + "'>'");
+    }
+
+
+    function cameraFail(message) {
+        alert("failure due to: " + message);
+    }
+}
